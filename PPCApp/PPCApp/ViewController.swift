27be724ee3c785f36da85f02923b5a8c6eb289b6
Nguyen Duy Duong <duyduong9xx@gmail.com>
2017-03-20 10:15:26 +0700

    Create new project

---
+++ PPCApp/PPCApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PPCApp
+//
+//  Created by Nguyen Duy Duong on 3/20/17.
+//  Copyright © 2017 Nguyen Duy Duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
