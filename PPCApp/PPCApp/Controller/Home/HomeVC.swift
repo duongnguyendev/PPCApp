0d98421e5d4b1565fec700b9413123a0d8ef31b8
Nguyen Duy Duong <duyduong9xx@gmail.com>
2017-03-22 16:58:32 +0700

    Update lib

--- PPCApp/PPCApp/Controller/Home/HomeVC.swift
+++ PPCApp/PPCApp/Controller/Home/HomeVC.swift
@@ -7,47 +7,17 @@
 //
 
 import UIKit
+import Photos
 
 class HomeVC: BaseVC {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
-        
         title = "Home"
-        // Do any additional setup after loading the view.
-        
-        let buttonPush  = UIButton(type: UIButtonType.system)
-        buttonPush.setTitle("Push", for: .normal)
-        buttonPush.addTarget(self, action: #selector(pushDetail), for: .touchUpInside)
-        
-        let buttonPresent  = UIButton(type: UIButtonType.system)
-        buttonPresent.setTitle("Present", for: .normal)
-        buttonPresent.addTarget(self, action: #selector(presentDetail), for: .touchUpInside)
-        
-        buttonPush.translatesAutoresizingMaskIntoConstraints = false
-        buttonPresent.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(buttonPush)
-        view.addSubview(buttonPresent)
-        
-        buttonPush.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
-        buttonPush.topAnchor.constraint(equalTo: view.topAnchor, constant: 64).isActive = true
-        buttonPush.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
-        buttonPush.heightAnchor.constraint(equalToConstant: 40).isActive = true
-
-        buttonPresent.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
-        buttonPresent.topAnchor.constraint(equalTo: buttonPush.bottomAnchor, constant: 10).isActive = true
-        buttonPresent.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
-        buttonPresent.heightAnchor.constraint(equalToConstant: 40).isActive = true
-    }
-    
-    func pushDetail() {
-        let detailVC = PostDetailVC()
-        self.push(viewController: detailVC)
-    }
-    func presentDetail(){
-        let detailVC = PostDetailVC()
-        self.present(viewController: detailVC)
+        let assets = PHAsset.fetchAssets(with: .image, options: nil)
+        PHImageManager.default().requestImage(for: assets[0], targetSize: CGSize(width: 1000, height: 1000), contentMode: .default, options: nil, resultHandler: { (image, nil) in
+            self.view.backgroundColor = UIColor.init(patternImage: image!)
+        })
     }
 }
